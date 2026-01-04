d8f0e5d4e87a7b185a51943d7ca23f8a945ab425
saikumarchittamalla770 <chittamallasaikumar957@gmail.com>
2026-01-04 19:57:26 +0530

    updated env

--- AI_BACKEND/src/config/env.js
+++ AI_BACKEND/src/config/env.js
@@ -1,5 +1,4 @@
 
 export const MODEL = process.env.MODEL || 'gpt-4o-mini';
-// export const OPENAI_KEY = process.env.OPENAI_API_KEY;
 export const WORKSPACE_ROOT = process.env.WORKSPACE_ROOT || '/mnt/data/workspcaces';
 // export const WORKSPACE_ROOT = process.env.WORKSPACE_ROOT || '/mnt/data/complete/';
